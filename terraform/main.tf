1f90851871d6a4d229a0a2694b87a64e9f28eebb
Max Netterberg <max.netterberg@gmail.com>
2023-07-18 15:53:49 +0200

    removed variable and added ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,7 @@ provider "kubernetes" {
 locals {
   namespace = "me"
   name      = "me"
+  hosts = ["max.netterberg.io"]
 }
 
 variable "image_tag" {
@@ -29,6 +30,55 @@ resource "kubernetes_namespace_v1" "me_ns" {
     name = local.namespace
   }
 }
+
+resource "kubernetes_ingress_v1" "me_ingress" {
+
+  metadata {
+    name      = local.name
+    namespace = local.namespace
+  }
+
+  spec {
+    dynamic "rule" {
+      for_each = toset(local.hosts)
+      content {
+        host = rule.value
+        http {
+          path {
+            backend {
+              service {
+                port {
+                  number = kubernetes_service_v1.me_service.spec[0].port[0].port
+                }
+                name = kubernetes_service_v1.me_service.metadata[0].name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_service_v1" "me_service" {
+  metadata {
+    name      = local.name
+    namespace = local.namespace
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment_v1.me_deploy.spec[0].selector[0].match_labels.app
+    }
+
+    port {
+      protocol    = "TCP"
+      port        = 3000
+      target_port = 3000
+    }
+  }
+}
+
 
 
 resource "kubernetes_deployment_v1" "me_deploy" {
